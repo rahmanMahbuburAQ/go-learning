77cd023bf2431672d706945fa43be7c990943d34
rahmanMahbuburAQ <rahman.muhammadmahbubur@asia-quest.jp>
2024-11-13 13:03:45 +0900

    receiver function with pointers

--- bill.go
+++ bill.go
@@ -12,14 +12,19 @@ type bill struct {
 func newBill(name string) bill {
 	b := bill{
 		name:  name,
-		items: map[string]float64{"pie": 5.99, "cake": 3.99},
+		items: map[string]float64{},
 		tip:   0,
 	}
 	return b
 }
 
+// add item to bill
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
+}
+
 // format the bill
-func (b bill) format() string {
+func (b *bill) format() string {
 	fs := "Bill breakdown:\n"
 	var total float64 = 0
 
@@ -29,8 +34,17 @@ func (b bill) format() string {
 		total += v
 	}
 
+	// add tip
+	fs += fmt.Sprintf("%-25v ...$%v\n", "tip:", b.tip)
+
 	// add total
-	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total+b.tip)
 
 	return fs
+}
+
+// update tip
+func (b *bill) updateTip(tip float64) {
+	(*b).tip = tip
+	// b.tip = tip
 }
