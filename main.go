d9988dc25b71027493e38a62fa7cf847f0e48513
rahmanMahbuburAQ <rahman.muhammadmahbubur@asia-quest.jp>
2024-11-13 12:55:58 +0900

    pointers

--- main.go
+++ main.go
@@ -2,36 +2,39 @@ package main
 
 import "fmt"
 
-// func updateName(x string) {
-// 	x = "yoshi"
+// func updateName(n string) {
+// 	n = "wedge"
 // }
 
-func updateName(x string) string {
-	x = "wedge"
-	return x
-}
-
-func updateMenu(y map[string]float64) {
-	y["coffee"] = 2.99
+func updateName(n *string) {
+	*n = "wedge"
 }
 
 func main() {
-	// group A types -> strings, ints, bools, floats, arrays, structs
-	// non-pointer wrapper values
 	name := "tifa"
 
 	// updateName(name)
-	name = updateName(name)
+	// fmt.Println(name)
 
+	// & gets the memory address of the value (pointer)
+	fmt.Println("memory address of name is:", &name)
+
+	// * gets the value at the specified memory address
+	m := &name
+	fmt.Println("memory address:", m)
+	fmt.Println("value at memory address:", *m)
+
+	updateName(m) // using pointer as arg, can pass &name directly instead of "m" as well
 	fmt.Println(name)
 
-	// group B types -> slices, maps, functions
-	// pointer wrapper values
-	menu := map[string]float64{
-		"pie":       5.95,
-		"ice cream": 3.99,
-	}
-
-	updateMenu(menu)
-	fmt.Println(menu)
 }
+
+/*
+
+|--name---|----m----|
+|  0x001  |  0x002  |
+|---------|---------|
+| "tifa"  | p0x001  |
+|---------|---------|
+
+*/
